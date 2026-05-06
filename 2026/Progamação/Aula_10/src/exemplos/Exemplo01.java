19bd69248676ec6d4afa6360532bc3fcfa3a2180
MichelSchauren <michel.schauren@aluno.feliz.ifrs.edu.br>
2026-05-06 11:42:30 -0300

    Aula_10

---
+++ 2026/Progamação/Aula_10/src/exemplos/Exemplo01.java
@@ -0,0 +1,32 @@
+package exemplos;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa();
+		p1.nome = "Ayrton";
+		p1.sobrenome = "Oliveira Manhães";
+		p1.idade = 16;
+	
+		Pessoa p2 = new Pessoa();
+		p2.nome = "Ayrton";
+		p2.sobrenome = "Oliveira Manhães";
+		p2.idade = 16;
+		
+		Pessoa chap = new Pessoa();
+		chap.nome = "Chapolin";
+		chap.sobrenome = "Colorado";
+		chap.idade = 67;
+		
+		p1.dizerOla();
+		
+		String ola = chap.getOla();
+		System.out.println(ola);
+		
+		int a = 2;
+		int b = 1;
+		String frase = String.format("%d + %d é %d. Não sei matemática. Sou um betinha!", a, b, p2.realizarSoma(a, b));
+		p2.falar(frase);
+	}
+}
---
+++ 2026/Progamação/Aula_10/src/exemplos/Pessoa.java
@@ -0,0 +1,31 @@
+package exemplos;
+
+public class Pessoa {
+	// Atributos
+	// Caracterizam a classe e determinam  estado
+	public String nome = "Jane";
+	public String sobrenome = "Doe";
+	public int idade = 0;
+	
+	// Métodos
+	// Comportamento da classe (ação)
+	public void dizerOla() {
+		System.out.printf("%s %s falou Olá!\n", this.nome, this.sobrenome);
+	}
+	
+	public String getOla() {
+		return this.nome + " " + this.sobrenome + " falou olá!\n";
+	}
+	
+	public void falar(String frase) {
+		System.out.printf("%s %s falou %s\n", this.nome, this.sobrenome, frase);
+	}
+	
+	public int realizarSoma(int n1, int n2) {
+		int soma = n1 + n2;
+		if (this.nome.equals("Ayrton")) {
+			soma++;
+		}
+		return soma;
+	}
+}
